22e0f06b6544912ddd1b655ee22b6b0d04fc6866
Cefn Hoile <github.com@cefn.com>
2021-07-14 00:28:44 +0100

    Inline styles in original SSR (#12)

--- src/components/Viewport.tsx
+++ src/components/Viewport.tsx
@@ -1,16 +1,41 @@
-import React, { FC } from 'react'
+import React, { FC } from "react";
+import ReactDOMServer from "react-dom/server";
 
-export const Viewport: FC = ({ children }) => (
-  <>
-    <style global jsx>
-      {`
-        html,
-        body,
-        #__next {
-          height: 100%;
-        }
-      `}
-    </style>
-    {children}
-  </>
-)
+import {
+  createMuiTheme,
+  ServerStyleSheets,
+  ThemeProvider
+} from "@material-ui/core/styles";
+import { Controls } from "./Controls";
+import { createStore } from "@lauf/store";
+import { AppState } from "../types";
+import { INITIAL_APPSTATE } from "../logic";
+
+const ssrStore = createStore<AppState>(INITIAL_APPSTATE);
+const sheets = new ServerStyleSheets();
+// Render the component to a string.
+ReactDOMServer.renderToString(
+  sheets.collect(
+    <ThemeProvider theme={createMuiTheme()}>
+      <Controls store={ssrStore} />
+    </ThemeProvider>
+  )
+);
+
+export const Viewport: FC = ({ children }) => {
+  return (
+    <>
+      <style id="jss-server-side">${sheets.toString()}</style>
+      <style global jsx>
+        {`
+          html,
+          body,
+          #__next {
+            height: 100%;
+          }
+        `}
+      </style>
+      {children}
+    </>
+  );
+};
