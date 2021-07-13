c7f7dc7809c4a62827d1df054106756dcad7b6b6
Cefn Hoile <github.com@cefn.com>
2021-07-14 00:33:15 +0100

    Remove spurious SSR CSS render

--- src/components/Viewport.tsx
+++ src/components/Viewport.tsx
@@ -1,31 +1,8 @@
 import React, { FC } from "react";
-import ReactDOMServer from "react-dom/server";
-
-import {
-  createMuiTheme,
-  ServerStyleSheets,
-  ThemeProvider
-} from "@material-ui/core/styles";
-import { Controls } from "./Controls";
-import { createStore } from "@lauf/store";
-import { AppState } from "../types";
-import { INITIAL_APPSTATE } from "../logic";
-
-const ssrStore = createStore<AppState>(INITIAL_APPSTATE);
-const sheets = new ServerStyleSheets();
-// Render the component to a string.
-ReactDOMServer.renderToString(
-  sheets.collect(
-    <ThemeProvider theme={createMuiTheme()}>
-      <Controls store={ssrStore} />
-    </ThemeProvider>
-  )
-);
 
 export const Viewport: FC = ({ children }) => {
   return (
     <>
-      <style id="jss-server-side">${sheets.toString()}</style>
       <style global jsx>
         {`
           html,
